0f20ed641311096f3382552e6683d0ef046c59f6
Hannah Paxton <hannahlinnpaxton@gmail.com>
2021-03-17 20:43:22 -0400

    Calling correct variables within the print statement

--- produce_summary.py
+++ produce_summary.py
@@ -1,15 +1,26 @@
+#print Day 1
 print("Day 1")
+#open the day 1 delivery log
 the_file = open("um-deliveries-20140519.txt")
+#iterate through the day 1 delivery log
 for line in the_file:
+    #strip the whitespace to the right of the line
     line = line.rstrip()
+    #split the items in the line by the "|" character
     words = line.split('|')
 
+    #select the first item in words
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    #select the first item in words, this will be changed to the 2nd
+    count = words[1]
+    #select the first item in words, this will be changed to the 3nd
+    amount = words[2]
 
+    #print statement passing through variables of count, melon, and amount
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
+
+#closing of the file
 the_file.close()
 
 
@@ -20,8 +31,8 @@ for line in the_file:
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
@@ -35,8 +46,8 @@ for line in the_file:
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
